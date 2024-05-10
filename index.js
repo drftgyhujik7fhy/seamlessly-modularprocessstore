0cbf52b003cf70467019b12d217564dc935139a1
drftgyhujik7fhy <js1shilledu@hotmail.com>
2024-05-10 13:26:02 +0700

    feat task

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-  function isValid(node, min, max) {
-    if (!node) return true;
-    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-      return false;
-    return (
-      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-    );
+function canCross(stones) {
+  const map = new Map();
+  for (const stone of stones) map.set(stone, new Set());
+  map.get(0).add(0);
+  for (let i = 0; i < stones.length; i++) {
+    for (const step of map.get(stones[i])) {
+      for (let k = step - 1; k <= step + 1; k++) {
+        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+      }
+    }
   }
+  return map.get(stones[stones.length - 1]).size > 0;
 }

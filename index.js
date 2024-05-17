a558efb4d624ba66962c4a3f45221cd8523d5fd4
drftgyhujik7fhy <js1shilledu@hotmail.com>
2024-05-18 01:49:00 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function combine(n, k) {
-  const result = [];
-  backtrack(1, []);
-  return result;
-  function backtrack(start, current) {
-    if (current.length === k) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = start; i <= n; i++) {
-      current.push(i);
-      backtrack(i + 1, current);
-      current.pop();
-    }
-  }
-}
+const uniqueValues = (arr) => [...new Set(arr)];
